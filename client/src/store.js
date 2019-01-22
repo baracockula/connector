ca3fb1431a4c4e65632768cbe8d36cff66f369c8
baracockula <baracockula@gmail.com>
2019-01-22 19:58:07 +0100

    Changed redux devtools extensins in store

--- client/src/store.js
+++ client/src/store.js
@@ -11,7 +11,12 @@ const store = createStore(rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    process.env.NODE_ENV === "production"
+      ? (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()) ||
+      compose
+      : compose
   )
 )
 
